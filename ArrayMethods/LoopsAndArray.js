659ad9aaa14eed4157f7034ea60b880c56bff535
Hanz Lagonero <hanzlagonero@gmail.com>
2025-12-13 12:54:19 +0800

    update ArrayMEthods

---
+++ ArrayMethods/LoopsAndArray.js
@@ -0,0 +1,11 @@
+function sumOfTripleEvents(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      const tripleEvenNumber = array[i] * 3;
+
+      sum += tripleEvenNumber;
+    }
+  }
+  return sum;
+}
