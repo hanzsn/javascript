1fe134e4b2879ecaa90963d2aa7f817e6320dc62
Hanz Lagonero <hanzlagonero@gmail.com>
2025-12-14 23:50:17 +0800

    update rps html and js

--- OdinProjects/src/rps.js
+++ OdinProjects/src/rps.js
@@ -1,83 +1,81 @@
-const playerChoice = document.querySelector(".playerChoice");
-const computerChoice = document.querySelector(".compChoice");
-const img = document.querySelectorAll(".img");
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
+const playerChoices = document.querySelectorAll(".playerChoices");
+const cRes = document.querySelector(".cRes");
+const pRes = document.querySelector(".pRes");
+const result = document.querySelector(".result");
 
-let total = 5;
-let player = 0;
-let computer = 0;
+let total = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-const playerImages = {
-  rock: "../img/leftRock.png",
-  paper: "../img/leftPaper.png",
-  scissor: "../img/leftScissor.png",
-};
-
-const computerImages = {
+const computerChoices = {
   rock: "../img/rightRock.png",
   paper: "../img/rightPaper.png",
   scissor: "../img/rightScissor.png",
 };
 
-const getComputerChoice = function () {
-  const keys = Object.keys(computerImages);
-  const randomIndex = Math.floor(Math.random() * keys.length);
-  const randomNum = keys[randomIndex];
-
-  return {
-    name: randomNum,
-    src: computerImages[randomNum],
-  };
+const playerChoicesIMG = {
+  rock: "../img/leftRock.png",
+  paper: "../img/leftPaper.png",
+  scissor: "../img/leftScissor.png",
 };
 
-img.forEach((choices) => {
-  choices.addEventListener("click", () => {
-    const computerChose = getComputerChoice();
-    const choice = choices.dataset.choice;
-    switch (choice) {
-      case "rock":
-        computerChoice.src = computerChose.src;
-        playerChoice.src = playerImages.rock;
-        if (choice === "rock" && computerChose.name === "scissor") {
-          player += 1;
-          alert(`player score: ${player}`);
-        } else if (choice === "rock" && computerChose.name === "paper") {
-          computer += 1;
-          alert(`computer score: ${computer}`);
-        }
-        break;
-      case "paper":
-        computerChoice.src = computerChose.src;
-        playerChoice.src = playerImages.paper;
-        if (choice === "paper" && computerChose.name === "rock") {
-          player += 1;
-          alert(`player score: ${player}`);
-        } else if (choice === "paper" && computerChose.name === "scissor") {
-          computer += 1;
-          alert(`computer score: ${computer}`);
-        }
-        break;
-      case "scissor":
-        computerChoice.src = computerChose.src;
-        playerChoice.src = playerImages.scissor;
-        if (choice === "scissor" && computerChose.name === "paper") {
-          player += 1;
-          alert(`player score: ${player}`);
-        } else if (choice === "scissor" && computerChose.name === "rock") {
-          computer += 1;
-          alert(`computer score: ${computer}`);
-        }
-        break;
+function getComputerChoice() {
+  let choices = Object.keys(computerChoices);
+  let randomize = Math.floor(Math.random() * choices.length);
+  let randomChoice = choices[randomize];
+
+  return {
+    name: randomChoice,
+    src: computerChoices[randomChoice],
+  };
+}
+
+function checkWinner() {
+  if (playerScore === 5) {
+    result.textContent = "Player Wins the Game! 🎉";
+    playerScore = 0;
+    computerScore = 0;
+    pRes.textContent = 0;
+    cRes.textContent = 0;
+    return true;
+  }
+  if (computerScore === 5) {
+    result.textContent = "Computer Wins the Game! 🤖";
+    playerScore = 0;
+    computerScore = 0;
+    pRes.textContent = 0;
+    cRes.textContent = 0;
+    return true;
+  }
+  return false;
+}
+
+playerChoices.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let choices = btn.dataset.choice;
+    let computerChoice = getComputerChoice();
+
+    player.src = playerChoicesIMG[choices];
+    computer.src = computerChoice.src;
+    result.textContent = `player: ${choices} & computer: ${computerChoice.name}`;
+
+    if (choices === computerChoice.name) {
+      result.textContent = "Draw!";
+      console.log("draw");
+    } else if (
+      (choices === "rock" && computerChoice.name === "scissor") ||
+      (choices === "paper" && computerChoice.name === "rock") ||
+      (choices === "scissor" && computerChoice.name === "paper")
+    ) {
+      pRes.textContent = ++playerScore;
+      console.log("player +1");
+    } else {
+      cRes.textContent = ++computerScore;
+      console.log("computer +1");
     }
 
-    if (player === total) {
-      alert("player wins!");
-      player = null;
-      computer = null;
-    }
-    if (computer === total) {
-      alert("computer wins!");
-      player = null;
-      computer = null;
-    }
+    checkWinner();
   });
 });
