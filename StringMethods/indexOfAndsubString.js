628b6cd87b3dfda71df6fa90863a9d2e20d6ef15
Hanz Lagonero <hanzlagonero@gmail.com>
2025-12-14 20:56:40 +0800

    Remove folder from repo

--- StringMethods/indexOfAndsubString.js
+++
@@ -1,7 +0,0 @@
-// index & subString()
-let text = "My email is mamiwowie@gmail.com";
-
-let start = text.indexOf("mamiwowie");
-let end = text.indexOf(".com") + 4;
-
-console.log(text.substring(start, end));
