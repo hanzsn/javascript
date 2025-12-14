2e4f25d881e414a1bc4ca692b6754d22dd25ed2e
Hanz Lagonero <hanzlagonero@gmail.com>
2025-12-14 20:55:22 +0800

    Remove folder from repo

--- ArrayMethods/find.js
+++
@@ -1,3 +0,0 @@
-const numbers = [1, 2, 3, 4];
-const firstEven = numbers.find((num) => num % 2 === 0);
-console.log(firstEven); // 2 (the first even number)
--- ArrayMethods/map.js
+++
@@ -1,5 +0,0 @@
-// Student Grades Doubler (Map)
-const studentGrades = [75, 88, 92, 60, 81, 95];
-
-const newArr = studentGrades.map((n) => n * 2);
-console.log(newArr);
